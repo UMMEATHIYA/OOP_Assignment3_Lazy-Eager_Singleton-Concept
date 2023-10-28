14a109c0545e110e2a025dc11a76df42e86fbe4d
Umme Athiya <43459908+UMMEATHIYA@users.noreply.github.com>
2023-10-27 20:06:37 -0500

    final commit files

--- OOP_Assignment3/Question1/EagerSingletonAssignment3.java
+++ OOP_Assignment3/Question1/EagerSingletonAssignment3.java
@@ -4,12 +4,20 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class EagerSingletonAssignment3 {
-    private static final int MAX_INSTANCES = 3;
-    private static int instanceCount = 0;
-    private static EagerSingletonAssignment3[] instances = new EagerSingletonAssignment3[MAX_INSTANCES];
 
+    // the variables are defined as private static because they prove a good example of encapsulation and these variables can be accessed within the class and static variables can be accessed with the help of class name.
+    private static final int MAXIMUM_INSTANCES = 3;
+    private static int inst_count = 0;
+    // this statement basically instantiates the number of instances to be created, also it holds an array of elements.
+    // In this case there are 3 instances as suggested in the question.
+    private static EagerSingletonAssignment3[] instances = new EagerSingletonAssignment3[MAXIMUM_INSTANCES];
+
+    // this is a static initialization block which creates the instances during the loading time.
+    // All the instances are created at one time even if the user may not be needing all the instances.
+    // This is good for exception handling.
+    // this block is traversing through the number of elements or the instances.
     static {
-        for (int i = 0; i < MAX_INSTANCES; i++) {
+        for (int i = 0; i < MAXIMUM_INSTANCES; i++) {
             instances[i] = new EagerSingletonAssignment3();
         }
     }
@@ -21,11 +29,15 @@ public class EagerSingletonAssignment3 {
     }
 
     public static EagerSingletonAssignment3 getInstance() {
-        if (instanceCount < MAX_INSTANCES) {
-            return instances[instanceCount++];
+        EagerSingletonAssignment3 result;
+        if (inst_count < MAXIMUM_INSTANCES) {
+            result = instances[inst_count++];
         } else {
             System.out.println("Maximum number of instances reached.");
-            return null;
+            result = null;
         }
+        return result;
+
+
     }
 }
--- OOP_Assignment3/Question1/LazySingletonAssignment3.java
+++ OOP_Assignment3/Question1/LazySingletonAssignment3.java
@@ -4,9 +4,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class LazySingletonAssignment3 {
-    private static final int MAX_INSTANCES = 3;
-    private static int instanceCount = 0;
-    private static LazySingletonAssignment3[] instances = new LazySingletonAssignment3[MAX_INSTANCES];
+    // the variables are defined as private static because they prove a good example of encapsulation and these variables can be accessed within the class and static variables can be accessed with the help of class name.
+    private static final int MAXIMUM_INSTANCES = 3;
+    private static int inst_count = 0;
+    // this statement basically instantiates the number of instances to be created, also it holds an array of elements.
+    // In this case there are 3 instances as suggested in the question.
+    // this creates the instance in global access method.
+    private static LazySingletonAssignment3[] instances = new LazySingletonAssignment3[MAXIMUM_INSTANCES];
 
     private LazySingletonAssignment3() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("H:mm:ss:S");
@@ -15,9 +19,12 @@ public class LazySingletonAssignment3 {
     }
 
     public static LazySingletonAssignment3 getInstance() {
-        if (instanceCount < MAX_INSTANCES) {
-            instances[instanceCount] = new LazySingletonAssignment3();
-            return instances[instanceCount++];
+        if (inst_count < MAXIMUM_INSTANCES) {
+            // unlike the Eager Singleton, here the instances are created only when there is a requirement.
+            // so there is a delay in creating the objects or instances.
+            // we notice the seconds and milliseconds difference.
+            instances[inst_count] = new LazySingletonAssignment3();
+            return instances[inst_count++];
         } else {
             System.out.println("Maximum number of instances reached.");
             return null;
